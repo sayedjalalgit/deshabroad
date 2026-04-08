f9172573e407f9f7eda29dd8c65d37973300f0aa
cosmowebsbd <cosmowebsbd@gmail.com>
2026-04-08 10:53:06 -0700

    fix prisma client import path

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,12 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
+import { PrismaClient } from '../generated/prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 function createPrismaClient() {
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
-  const adapter = new PrismaPg(pool)
+  const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
   return new PrismaClient({ adapter })
 }
 
